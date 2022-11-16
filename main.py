2a68f6982db12dec7edd32c624ae3eff026cccb6
LoXa <hugo.degioanni@gmail.com>
2022-11-16 09:34:06 +0100

    fin du corps

---
+++ main.py
@@ -0,0 +1,5 @@
+from persos import*
+a=input("quel est ton nom jeune aventurier")
+perso=Protagonistes(a,4,20)
+mechant=Protagonistes("valentin",5,30)
+perso.attaquer(mechant)
--- persos.py
+++ persos.py
@@ -1,1 +1,21 @@
-class Protagonistes
+class Protagonistes:
+    def __init__ (self,nom=str,force=int,pv=int):
+        self.nom=nom
+        self.force=force
+        self.pv=pv
+        
+    def attaquer(self,cible):
+        cible.pv=cible.pv-self.force
+        print(f"{self.nom} a attaqué {cible.nom} qui a perdu {self.force} pv. Il est maintenant à {cible.pv} pv")
+        return cible.pv
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
