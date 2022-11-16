b47f76cebf3541333a6ac2386657012fefe59cf2
LoXa <hugo.degioanni@gmail.com>
2022-11-16 11:00:25 +0100

    Premiere session

--- persos.py
+++ persos.py
@@ -1,3 +1,5 @@
+#https://excalidraw.com/#room=c32be8b72ac507f85b11,_00DXrB_vDc1R_d6D3GfZw
+
 class Protagonistes:
     def __init__ (self,nom=str,force=int,pv=int):
         self.nom=nom
@@ -10,7 +12,20 @@ class Protagonistes:
         return cible.pv
     
     
-    
+class Sorcier:
+    def __init__(self,mana=int,potions=int):
+        self.mana=mana
+        self.potions=potions
+        
+    def soin(self):
+        if self.mana>5 and self.potions>1:
+            self.pv+=30/100*self.pv
+            self.mana-=5
+            self.potions-=1
+            
+class Chevalier:
+    def __init__(self,bouclier_dura=int):
+        self.bouclier_dura=bouclier_dura
     
     
     
