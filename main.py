48fd9caa8b3ef2639085b268540b7d378512d4a5
LoXa <hugo.degioanni@gmail.com>
2022-11-21 11:01:04 +0100

    régler choix classe en integrant l'initialisation

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from os import system
+import platform
+def clear():
+    if platform.system()== "Windows":
+        system('cls')
+    else:
+        system('clear') 
+        
+from time import sleep
 from persos import*
-a=input("quel est ton nom jeune aventurier")
-perso=Protagonistes(a,4,20)
+a=input("quel est ton nom jeune aventurier\n")
+test=Protagonistes(a,4,20)
+test.choix_classe
+perso=Protagonistes(classe)
+clear()
+sleep(2)
+perso.choix_classe
+print(f"L'aventure du jeune {perso.nom} commence")
+sleep(2)
+clear()
 mechant=Protagonistes("valentin",5,30)
 perso.attaquer(mechant)
+
+#definir random les potions du sorcier
--- persos.py
+++ persos.py
@@ -1,15 +1,29 @@
 #https://excalidraw.com/#room=c32be8b72ac507f85b11,_00DXrB_vDc1R_d6D3GfZw
 
 class Protagonistes:
-    def __init__ (self,nom=str,force=int,pv=int):
+    def __init__ (self,classe,nom=str,force=int,pv=int):
         self.nom=nom
         self.force=force
         self.pv=pv
+        self.classe=classe
         
     def attaquer(self,cible):
         cible.pv=cible.pv-self.force
         print(f"{self.nom} a attaqué {cible.nom} qui a perdu {self.force} pv. Il est maintenant à {cible.pv} pv")
         return cible.pv
+    def choix_classe(self):
+        choix = int(input("Quelle classe voulez-vous incarner ? 1 : Sorcier \n 2 : Chevalier \n 3 : Elfe"))
+        match choix:
+            case 1:
+                classe=Sorcier(20,2)
+                print("vous êtes désormais un sorcier")
+                return classe
+            case 2:
+                classe=Chevalier(10)
+                print("vous êtes désormais un Chevalier")
+                return classe
+            case 3:
+                print("tg")
     
     
 class Sorcier:
@@ -25,8 +39,11 @@ class Sorcier:
             
 class Chevalier:
     def __init__(self,bouclier_dura=int):
-        self.bouclier_dura=bouclier_dura
-    
+        self.bouclier_dura=bouclier_dura 
+    def bloquer(self,cible):
+        self.pv+=cible.force
+        self.bouclier_dura-=5
+        print(f"{self.nom} se prépare à parer la prochaine attaque")
     
     
     
